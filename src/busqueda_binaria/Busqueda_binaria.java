e85f05bbffe59484c21d4939fa57a406767fb237
Marcos <marcosphsevi@gmail.com>
2026-02-13 22:53:08 +0100

    depurando el codigo

--- src/busqueda_binaria/Busqueda_binaria.java
+++ src/busqueda_binaria/Busqueda_binaria.java
@@ -8,45 +8,66 @@ public class Busqueda_binaria {
 		Random rand = new Random();
 		Scanner sc = new Scanner(System.in);
 		int [] array = new int [50];
-		
+
 		for (int i = 0; i < array.length;) {
-		    boolean repetido = false;
-		    array[i] = rand.nextInt(200);
-		    for (int j = 0; j < i; j++) {
-		        if (array[i] == array[j]) {
-		            repetido = true;	            
-		        }
-		    }
-		    if (!repetido) i++;
+			boolean repetido = false;
+			array[i] = rand.nextInt(200);
+			for (int j = 0; j < i; j++) {
+				if (array[i] == array[j]) {
+					repetido = true;	            
+				}
+			}
+			if (!repetido) i++;
 		}
-		
+
 		Arrays.sort(array);
-		
+
 		System.out.println("Introduce un número, entre el 0 y el 200: ");
-		int numero = sc.nextInt();
-		
+		int numero = comprobacionNumeroEntero(sc, sc.nextLine());
+
 		int inicio = 0;
 		int fin = array.length - 1;
 		boolean posicionEncontrada = false; 
 		int mitad = 0;
-		
+
 		while (inicio <= fin && !posicionEncontrada) {
-		    mitad = inicio + (fin - inicio) / 2;
-		    if (array[mitad] == numero) {
-		        posicionEncontrada = true;
-		    }
-		    if (numero < array[mitad]) {
-		        fin = mitad - 1;
-		    } else {
-		        inicio = mitad + 1;
-		    }
+			mitad = inicio + (fin - inicio) / 2;
+			if (array[mitad] == numero) {
+				posicionEncontrada = true;
+			}
+			if (numero < array[mitad]) {
+				fin = mitad - 1;
+			} else {
+				inicio = mitad + 1;
+			}
 		}
-		
+
 		if (posicionEncontrada) {
-		    System.out.println("El número " + numero + " se encuentra en la posición " + mitad);
+			System.out.println("El número " + numero + " se encuentra en la posición " + mitad);
 		} else {
-		    System.out.println("El número no está en el array");
+			System.out.println("El número no está en el array");
 		}
 		sc.close();
+	}
+
+
+
+	public static int comprobacionNumeroEntero (Scanner sc, String num) {
+		int numero = 0;
+		while (true) {
+			try {
+				numero = Integer.parseInt(num);
+				if (numero >= 0 && numero <= 200) {
+					break;
+				}else {
+					System.out.println("El número tiene que estar comprendido entre " + 0 + " y " + 200);
+					num = sc.nextLine();
+				}
+			} catch (NumberFormatException e) {
+				System.out.println("El número tiene que ser entero, inténtelo otra vez.");
+				num = sc.nextLine();
+			}
+		}
+		return numero;
 	}
 }
